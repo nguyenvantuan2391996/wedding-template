3d15287afbbd2c84298c4f6dd2ff0f5c0b7f6837
tuannguyen <nguyenvantuan2391996@gmail.com>
2024-02-21 22:32:00 +0700

    feat: wedding

--- src/Components/Music.tsx
+++ src/Components/Music.tsx
@@ -2,17 +2,17 @@ import React, { useEffect, useState } from "react";
 import MusicMP3 from "../Assets/Music.mp3";
 
 const MusicPlayer = () => {
-  const [isPlaying, setIsPlaying] = useState(false);
-  useEffect(() => {
-    let audio = new Audio(MusicMP3);
-    audio.loop = true;
-    audio.play().catch(function (error) {
-      console.log(
-        "Chrome cannot play sound without user interaction first" + error
-      );
-    });
-  }, [isPlaying]);
-
+  // const [isPlaying, setIsPlaying] = useState(false);
+  // useEffect(() => {
+  //   let audio = new Audio(MusicMP3);
+  //   audio.loop = true;
+  //   audio.play().catch(function (error) {
+  //     console.log(
+  //       "Chrome cannot play sound without user interaction first" + error
+  //     );
+  //   });
+  // }, [isPlaying]);
+  //
   return <div></div>;
 };
 
